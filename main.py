ec0dc55b26631f0b36d41423a030871591e01ed3
Fehp <fernandop09@hotmail.com>
2021-03-06 19:48:22 -0300

    Created get specific book

--- main.py
+++ main.py
@@ -37,5 +37,19 @@ def api_all():
     return jsonify(books)
 
 
+@app.route("/api/v1/resources/books", methods=["GET"])
+def api_id():
+    if "id" in request.args:
+        id = int(request.args["id"])
+    else:
+        return"ERROR: no id field provided. Please specify id"
+
+    results = []
+    for book in books:
+        if book["id"] == id:
+            results.append(book)
+    return jsonify(results)
+
+
 if __name__ == "__main__":
     app.run()
